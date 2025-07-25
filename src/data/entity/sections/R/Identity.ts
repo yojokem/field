f3de22a0e1bb850e26830bfcbb4195a015d98595
Frost <yojokem@naver.com>
2025-07-25 15:43:45 +0900

    Fifteen-Eight-Two

--- src/data/entity/sections/R/Identity.ts
+++ src/data/entity/sections/R/Identity.ts
@@ -1,13 +1,17 @@
-import { Entity } from "typeorm";
+import { Column, Entity, JoinTable } from "typeorm";
 import { Fence } from "../../Fence";
 import { Address, AddressHolder } from "../Address";
 
 @Entity({name: "identity"})
 export class Identity extends Fence implements AddressHolder {
+    @Column()
     fullname: string;
+
+    @Column()
+    @JoinTable()
     resiRegAbs: Address[]; //주민 등록 초본
 
     id() {
-        return this.fid + "―" + this. + "|" + this.uid
+        return "[ID] " + this.fid + "―" + this.fullname + "|" + this.uid;
     }
 }
